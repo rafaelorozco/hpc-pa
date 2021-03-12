c27b6ee17ded3110336717a59fcaae641ccc5504
Orozco_Rafael_rorozcom3 <rorozcom3>
2021-03-12 18:00:57 -0500

    first implementation of scatter and broadcast

--- PA2/mpi_evaluator.cpp
+++ PA2/mpi_evaluator.cpp
@@ -10,14 +10,71 @@
 #include "const.h"
 
 void scatter(const int n, double* scatter_values, int &n_local, double* &local_values, int source_rank, const MPI_Comm comm){
-    //Implementation
 
+	int p, rank;
+    MPI_Comm_size(comm, &p);
+    MPI_Comm_rank(comm, &rank);
+
+    //set my own local values if I am source_rank
+    if(rank == source_rank) {
+    	n_local = n/p;
+    	local_values = (double *) malloc(n*sizeof(double))
+    	memcpy(local_values, scatter_values, n_local * sizeof(double));
+    }
+
+    MPI_Status stat;
+    if(rank == source_rank) {
+        for(int j = 1; j < p; j++) {
+            //First send the amount of numbers to send. 
+            MPI_Send(&n_local, 1, MPI_INT, j, 11, comm);
+
+            //First send the amount of numbers to send. 
+            MPI_Send(&scatter_values[j * n_local], n_local, MPI_DOUBLE, j, 12, comm);
+        }
+    } else {
+        //First rec the amount of numbers to rec. 
+        MPI_Recv(&n_local, 1, MPI_INT, source_rank, 11, comm, &stat);
+
+        //Allocate the memory
+        local_values = (double *) malloc(n*sizeof(double))
+        MPI_Recv(local_values, n_local, MPI_DOUBLE, source_rank, 12, comm, &stat);
+    }
 }
 
 double broadcast(double value, int source_rank, const MPI_Comm comm){
-    //Implementation
+	int p, rank;
+    MPI_Comm_size(comm, &p);
+    MPI_Comm_rank(comm, &rank);
 
-    return 0;
+    //Dimension of hypercubic communication
+    int d = log2(p);
+
+    //check if p = 2^d
+    if((1 << (d)) != p ) {
+    	d++;
+    }
+
+    MPI_Status stat;
+    int flip = 1 << (d-1);
+    int mask = flip - 1;
+    for (int j = d-1; j>=0 ;j--) {
+	    if((rank & mask) == 0) { //check if I am participating in this communic
+	    	if((rank & flip) == 0) { //check if I send
+	    		if((rank ^ flip) < p) {
+	    			
+	    			MPI_Send(&value, 1, MPI_DOUBLE, rank ^ flip, 11, comm);
+	    		}
+	    	} else {
+	    		if((rank ^ flip) < p) {
+	    		    //printf("receive my num from %i\n", rank ^ flip);
+	    			MPI_Recv(&value, 1, MPI_DOUBLE, rank ^ flip, 11, comm, &stat);
+	    		}
+	    	}     
+	    } 
+	    mask = mask >> 1;
+	    flip = flip >> 1;
+	}
+    return value;
 }
 
 void parallel_prefix(const int n, const double* values, double* prefix_results, const int OP, const MPI_Comm comm){
--- PA2/scatter.cpp
+++ PA2/scatter.cpp
@@ -11,60 +11,94 @@ int main(int argc, char *argv[]) {
     MPI_Comm comm = MPI_COMM_WORLD;
 
     int p, rank;
+    int source_rank = 0;
 
     MPI_Comm_size(comm, &p);
     MPI_Comm_rank(comm, &rank);
 
-    printf("all random numbers are: \n");
-    fflush()
-   
+    printf("Im rank %d\n",rank);
+
     //Get input arguments. 
     int  N; 
     long C;
 
     N = atoi(argv[1]);
+    C = atoi(argv[2]);
 
-    int proc_load = N/p;
+    int proc_load; 
    
     //Dimension of hypercubic communication
     int d = log2(p);
 
-    double all_nums[N];
-    double my_nums[proc_load];
+    double scatter_values[N];
+    double *local_values;
+    //double local_values[4];
 
-    for(int i = 0; i < N; i++) {
-        all_nums[i] = drand48();
-    }
+
+    //generate N/p random numbers
+    srand48(C+rank);
+
+    
 
     if(rank == 0) {
+        for(int i = 0; i < N; i++) {
+            scatter_values[i] = drand48();
+        }
+
+        proc_load = N/p;
+
         printf("all random numbers are: \n");
         fflush(stdout);
         for(int i = 0; i < N; i++) {
-            printf("%f", all_nums[i]);
+            printf("%f", scatter_values[i]);
         }
-        //Copy the n/p numbers into the rank 0 array
-        memcpy(my_nums, all_nums, proc_load * sizeof(double));
+      
     }
     printf("\n");
 
+    int n = N;
+
+    //set my own local values if I am source_rank
+    if(rank == source_rank) {
+        local_values = (double *) malloc(proc_load*sizeof(double));
+        memcpy(local_values, scatter_values, proc_load * sizeof(double));
+    }
+
     MPI_Status stat;
-    if(rank == 0) {
+    if(rank == source_rank) {
         for(int j = 1; j < p; j++) {
-            //printf("send my numbers to %i\n",  j);
-            MPI_Send(&all_nums[j * proc_load], proc_load, MPI_DOUBLE, j, 11, MPI_COMM_WORLD);
-            
+            //First send the amount of numbers to send. 
+            MPI_Send(&proc_load, 1, MPI_INT, j, 11, comm);
+
+            //First send the amount of numbers to send. 
+            MPI_Send(&(scatter_values[j * proc_load]), proc_load, MPI_DOUBLE, j, 12, comm);
         }
     } else {
-        //printf("receive my numbers from 0");
-        MPI_Recv(&my_nums, proc_load, MPI_DOUBLE, 0, 11, MPI_COMM_WORLD,&stat);
+        //First rec the amount of numbers to rec. 
+        MPI_Recv(&proc_load, 1, MPI_INT, source_rank, 11, comm, &stat);
+        printf("Got proc load as %d\n",proc_load);
+        printf("ITTm rank %d\n",rank);
+        //Allocate the memory
+        local_values = (double *) malloc(proc_load*sizeof(double));
+        MPI_Recv(local_values, proc_load, MPI_DOUBLE, source_rank, 12, comm, &stat);
+
+        printf("ITTm rank %d\n",rank);
+        printf("my numbers are: \n");
+        for(int i = 0; i < proc_load; i++) {
+            printf("%f", local_values[i]);
+        }
+        printf("\n");
+
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
+    printf("proc load as %d\n",proc_load);
+
     printf("Im rank %d\n",rank);
     printf("my numbers are: \n");
     for(int i = 0; i < proc_load; i++) {
-        printf("%f", my_nums[i]);
+        printf("%f", local_values[i]);
     }
     printf("\n");
     
