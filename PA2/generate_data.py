fc2d987f0b28d74f815aef006bcc6595926a7143
Orozco_Rafael_rorozcom3 <rorozcom3>
2021-03-12 22:40:54 -0500

    sample constants 10^6

--- PA2/generate_data.py
+++ PA2/generate_data.py
@@ -1,12 +1,12 @@
 from numpy import random
 
-n = 100
+n = 100000
 
 file1 = open("sample-constants"+str(n)+".txt","w") 
 
 file1.write(str(n))
 file1.write("\n") 
 for i in range(n):
-	file1.write(str(random.randint(100)) )
+	file1.write(str(random.randint(10)) )
 	file1.write("\n") 
 file1.close()
