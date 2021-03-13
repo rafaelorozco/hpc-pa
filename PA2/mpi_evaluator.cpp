06d04840649cc49d855e93943fbfa31216c52104
Francis <ziyi.yin.97@gmail.com>
2021-03-12 20:29:44 -0500

    fix

--- PA2/mpi_evaluator.cpp
+++ PA2/mpi_evaluator.cpp
@@ -8,7 +8,6 @@
 
 #include "mpi_evaluator.h"
 #include "const.h"
-#include <math.h>
 
 void scatter(const int n, double* scatter_values, int &n_local, double* &local_values, int source_rank, const MPI_Comm comm){
 
